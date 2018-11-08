b7dc58a98d7a8bcfab9820c265c4d04c5f194991
Vasilije Blaženi <vasilije.blazeni@gmail.com>
2018-11-08 08:01:47 +0100

    V0.1

--- cid/src/cid.awk
+++ cid/src/cid.awk
@@ -1,5 +1,109 @@
 #! /usr/bin/gawk -f
 
+C_program = "cid"
+
+func duzina_tipa_podatka( format,     )
+{
+	if( strmatch
+match(STRING, REGEXP [, ARRAY])'
+     The `match' function searches STRING for the longest, leftmost
+     substring matched by the regular expression, REGEXP.  It returns
+     the character position, or "index", at which that substring begins
+     (one, if it starts at the beginning of STRING).  If no match is
+     found, it returns zero.
+
+	if( match( format, /%(c|hh[duoxX])/ )
+		print "%c"
+	else if( match( format, /%[[:digit:]c/ )
+	{
+		print "%c"
+	}
+		
+		    "%(((h{0,2})[duoxX])|(l|L)?f|[[:digit:]]c)";
+
+	if( getline duzina_tipa[ format ] < c_program )
+
+	if( sscanf( p, "%hdc", &br_kar ) == 1 )	// %nc
+		return br_kar;
+
+	if( *p == 'c' || strncmp( p, "hh", 2 ) == 0 )	// p, %hhd, %hhu, %hho, %hhx, %hhX
+		return sizeof( char );
+
+	if( *p == 'h' )	// %hd, %hu, %ho, %hx, %hX
+		return sizeof( short );
+
+	if( strchr( "duoxX", *p ) != NULL )	// %d, %u, %o, %x, %X
+		return sizeof( int );
+
+	if( strcmp( p, "ld" ) == 0 )
+		return sizeof( long );
+
+	if( *p == 'l' )	// %la, %lA, %le, %lE, %lf, %lF, %lg, %lG
+		return sizeof( float );
+
+	if( *p == 'l' )	// %lf
+		return sizeof( float );
+
+	if( strchr( "aAeEfFgG", *p ) != NULL )	// %a, %A, %e, %E, %f, %F, %g, %G
+
+		return sizeof( int );
+
+	if( *p == 'L' )	// %La, %LA, %Le, %LE, %Lf, %LF, %Lg, %LG
+		return sizeof( long double );
+
+	errx( EXIT_FAILURE, "%s(): GRESKA: Nepostojeci specifikator formata %s\n",
+		__func__, format );
+
+
+
+
+
+
+				if( strcmp( format, "%c" ) == 0 )
+				else if( strcmp( format, "%hhu" ) == 0 )
+				else if( strcmp( format, "%hd" ) == 0 )
+				else if( strcmp( format, "%hu" ) == 0 )
+				else if( strcmp( format, "%d" ) == 0 )
+				else if( strcmp( format, "%u" ) == 0 )
+				else if( strcmp( format, "%ld" ) == 0 )
+				else if( strcmp( format, "%f" ) == 0 )
+				else if( strcmp( format, "%lf" ) == 0 )
+				else if( strcmp( format, "%Lf" ) == 0 )
+
+
+
+
+
+
+
+
+
+	print format |& C_program
+	C_program |& getline
+}
+
+
+func otvori_datoteku( datoteka )
+{
+	print datoteka |& C_program
+	odziv = ( C_program |& getline bafer )
+
+	if( !( odziv == 1 && bafer == "EOK" ) )
+	{
+		print "\notvori_datoteku(", datoteka, "): GRESKA: " > "/dev/stderr"
+
+		if( odziv == 1 )	// bafer != "EOK"
+			print "nema EOK\n\n" > "/dev/stderr"
+		else if( odziv == -1 )
+			print ERRNO "\n" > "/dev/stderr"
+		else
+			print "EOF\n" > "/dev/stderr"
+
+		exit
+	}
+}
+
+
 BEGIN	\
 {
 	for( i = 0; i < ARGC; i++ )
--- cid/src/cid.c
+++ cid/src/cid.c
@@ -8,10 +8,112 @@
  ============================================================================
  */
 
+#define VERZIJA "V0.1   08.11.2018."
+
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void) {
-	puts(""); /* prints  */
+
+unsigned char
+duzina_tipa_podatka( char format[] )
+{	// Podrazumeva se da je pri dodeli vrednosti nizu naredaba za one koje predstavljaju format ("%...") izvrsena provera i da su svi formati ispravni.
+	char	*p = format + 1;	// preskakanje '%' na pocetku specifikatora formata
+	short	br_kar;
+
+
+	if( sscanf( p, "%hdc", &br_kar ) == 1 )	// %nc
+		return br_kar;
+
+	if( *p == 'c' || strncmp( p, "hh", 2 ) == 0 )	// p, %hhd, %hhu, %hho, %hhx, %hhX
+		return sizeof( char );
+
+	if( *p == 'h' )	// %hd, %hu, %ho, %hx, %hX
+		return sizeof( short );
+
+	if( strchr( "duoxX", *p ) != NULL )	// %d, %u, %o, %x, %X
+		return sizeof( int );
+
+	if( strcmp( p, "ld" ) == 0 )
+		return sizeof( long );
+
+	if( *p == 'l' )	// %la, %lA, %le, %lE, %lf, %lF, %lg, %lG
+		return sizeof( float );
+
+	if( *p == 'l' )	// %lf
+		return sizeof( float );
+
+	if( strchr( "aAeEfFgG", *p ) != NULL )	// %a, %A, %e, %E, %f, %F, %g, %G
+
+		return sizeof( int );
+
+	if( *p == 'L' )	// %La, %LA, %Le, %LE, %Lf, %LF, %Lg, %LG
+		return sizeof( long double );
+
+	errx( EXIT_FAILURE, "%s(): GRESKA: Nepostojeci specifikator formata %s\n",
+		__func__, format );
+}
+
+
+int main( void )
+{
+	char	red[ 1000 ];
+	FILE	*pok_dat;
+
+
+puts("Pocetak\n"); /* prints  */
+
+	while( 1 )
+	{
+		if( fgets( red, sizeof red, pok_dat ) == NULL )
+		{
+			if (feof( fp )) {
+			              // We're at the end of the file.
+			              return EXIT_SUCCESS;
+			          } else {
+			              // An error occurred.
+			              perror ("fgets()");
+			              return EXIT_FAILURE;
+			          }
+		      puts( buffer );
+
+		}
+
+
+		if( strcmp ( red, "-p\n" ) == 0 )
+		{
+		}
+
+		// Obrada zahteva za veličinu tipova podataka:
+
+		else if( strcmp ( red, "%c\n" ) == 0 )
+			printf( "%d", sizeof( char ) );
+		else if( strcmp ( red, "%hd\n" ) == 0 )
+			printf( "%d", sizeof( short ) );
+		else if( strcmp ( red, "%d\n" ) == 0 )
+			printf( "%d", sizeof( int ) );
+		else if( strcmp ( red, "%ld\n" ) == 0 )
+			printf( "%d", sizeof( long ) );
+		else if( strcmp ( red, "%t\n" ) == 0 )
+			printf( "%d", sizeof( time_t ) );
+		else if( strcmp ( red, "%f\n" ) == 0 )
+			printf( "%d", sizeof( float ) );
+		else if( strcmp ( red, "%lf\n" ) == 0 )
+			printf( "%d", sizeof( double ) );
+		else if( strcmp ( red, "%Lf\n" ) == 0 )
+			printf( "%d", sizeof( long double ) );
+
+		// Obrada zahteva za otvaranje/obrazovanje datoteke:
+
+		else
+		{	// ime datoteke
+			red[ strlen( red ) - 1 ] = '\0';	// izbacivanje '\n' s kraja reda
+
+			if( ( pok_dat = fopen( red, "r+" ) == NULL ) {
+			      perror ("fopen()");
+			      return EXIT_FAILURE;
+			  }
+		}
+	}
+
 	return EXIT_SUCCESS;
 }
